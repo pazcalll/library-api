6419cd02666e040fcb187ca0deb30e1a0536569c
pazcalll <yazeedarifin@gmail.com>
2024-10-16 22:28:06 +0700

    feat: books and authors migration

--- database/migrations/2024_10_16_152629_create_books_table.php
+++ database/migrations/2024_10_16_152629_create_books_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Author;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,11 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('authors', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('bio');
-            $table->timestamp('birth_date');
+            $table->foreignIdFor(Author::class);
+            $table->string('title');
+            $table->string('description');
+            $table->timestamp('publish_date');
             $table->timestamps();
         });
     }
@@ -25,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('authors');
+        Schema::dropIfExists('books');
     }
 };
