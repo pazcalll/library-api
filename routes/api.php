2b24e72c43650e1bf836f72a9c3bd53e4694e8ff
pazcalll <yazeedarifin@gmail.com>
2024-10-17 00:06:33 +0700

    refactor: controllers

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\Api\AuthorController;
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::apiResource('authors', AuthorController::class);
+Route::apiResource('books', BookController::class);
